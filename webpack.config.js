51dbf5c2c846ea66d7088b3ca6978f4024019977
Dmytro Daskevych <daskevich1122@gmail.com>
2024-03-23 21:48:25 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "bundle.js",
   },
   resolve: {
@@ -30,6 +31,18 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public/images",
+          to: "images",
+        },
+        {
+          from: "public/logo",
+          to: "logo",
+        },
+      ],
     }),
   ],
 };
