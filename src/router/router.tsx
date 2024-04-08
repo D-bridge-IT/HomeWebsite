b2641c721c5cbcf56a2ec6da3979e11e83a32600
Dmytro Daskevych <daskevich1122@gmail.com>
2024-04-08 03:56:37 +0200

    fixes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import About from "../pages/About/About";
-import Contact from "../pages/Contact/Contact";
 import Home from "../pages/Home/Home";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/HomeWebsite/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 };
