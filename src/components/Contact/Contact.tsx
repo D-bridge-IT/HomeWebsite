d2f7207a5ab4e7ac4246227f764ee6fced2af4d7
Dmytro Daskevych <daskevich1122@gmail.com>
2024-04-11 15:07:23 +0200

    fix map

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -71,7 +71,7 @@ const Contact = () => {
             </div>
             <div className="contactWrapper__content__rightSide__mapContainer__smallMap">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1250.278656647958!2d6.442292539019733!3d51.190381673969895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8acbdf36d30d7%3A0xd069961a1b9d1516!2sOststra%C3%9Fe%2012%2C%2041065%20M%C3%B6nchengladbach!5e0!3m2!1sru!2sde!4v1710966151204!5m2!1sru!2sde"
+                src="https://maps.google.com/maps?width=350&amp;height=200&amp;hl=en&amp;q=Wulfshofstra%C3%9Fe%201,%20%2044149%20Dortmund+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                 width="350"
                 height="200"
                 loading="lazy"
